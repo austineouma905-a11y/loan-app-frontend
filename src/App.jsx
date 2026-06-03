7398a20e54107c24f63bbaf278dcbc5011f3a553
Austine Ouma <austinouma905@gmail.com>
2026-06-03 09:31:33 +0300

    fix: change the business name

--- src/App.jsx
+++ src/App.jsx
@@ -299,7 +299,7 @@ export default function App() {
               {isMenuOpen ? '✕' : '☰'}
             </button>
           )}
-          <h1><span className="text-blue">BUSINESS</span> & <span className="text-red">LOAN</span></h1>
+          <h1><span className="text-blue">AUSTINE'S</span><span className="text-red">LOAN BUSINESS</span></h1>
         </div>
         {isLoggedIn && (
           <div className="header-right-nav">
